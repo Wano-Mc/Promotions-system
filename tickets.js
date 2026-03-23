49ebbb5c7f4baec29d44293fe43897626607b031
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-24 00:13:00 +0300

    Update tickets.js - New ticket added

--- tickets.js
+++ tickets.js
@@ -1,50 +1,18 @@
 // بيانات التكتات
 window.ticketsData = [
   {
-    "timestamp": "2026-03-23T19:36:13.467Z",
-    "ticketOwner": "<@1162350243552170044>",
-    "ticketOwnerId": "1162350243552170044",
-    "ticketName": "youtuber・waiting-2",
-    "panelName": "Youtuber Apply",
-    "transcriptFile": "transcripts/transcript-youtuberwaiting-2.html",
+    "timestamp": "2026-03-23T21:12:57.405Z",
+    "ticketOwner": "<@1350531070222794804>",
+    "ticketOwnerId": "1350531070222794804",
+    "ticketName": "dc・waiting-49",
+    "panelName": "Dc Support",
+    "transcriptFile": "transcripts/transcript-dcwaiting-49.html",
     "claimedBy": null,
     "users": [
-      "<@1162350243552170044>",
       "<@722196398635745312>",
-      "<@1190305586710073427>"
-    ],
-    "ticketOwnerName": "emr.15",
-    "ticketOwnerDisplay": "๛MR々IVAR"
-  },
-  {
-    "timestamp": "2026-03-17T17:17:15.975Z",
-    "ticketOwner": "<@1226280439560536125>",
-    "ticketOwnerId": "1226280439560536125",
-    "ticketName": "closed-51",
-    "panelName": "Wihitelist Application",
-    "transcriptFile": "transcripts/transcript-closed-51.html",
-    "claimedBy": null,
-    "users": [
-      "<@557628352828014614>",
       "<@1350531070222794804>"
     ],
-    "ticketOwnerName": "f7a0",
-    "ticketOwnerDisplay": "Waleed"
-  },
-  {
-    "timestamp": "2026-03-17T00:05:07.627Z",
-    "ticketOwner": "<@790829096929067018>",
-    "ticketOwnerId": "790829096929067018",
-    "ticketName": "closed-50",
-    "panelName": "Wihitelist Application",
-    "transcriptFile": "transcripts/transcript-closed-50.html",
-    "claimedBy": null,
-    "users": [
-      "<@557628352828014614>",
-      "<@794290257243406348>",
-      "<@790829096929067018>"
-    ],
-    "ticketOwnerName": "h7mnay",
-    "ticketOwnerDisplay": "h7mnay"
+    "ticketOwnerName": "agent_omar.dev",
+    "ticketOwnerDisplay": "AGENT O.M.A.R"
   }
 ];
