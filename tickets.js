34f02298441a9d8718d45ccc5ac87294017deeef
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-24 19:10:27 +0300

    Update tickets.js - New ticket added

--- tickets.js
+++ tickets.js
@@ -1,6 +1,22 @@
 // بيانات التكتات
 window.ticketsData = [
   {
+    "timestamp": "2026-03-24T16:10:24.976Z",
+    "ticketOwner": "<@1330898742110457962>",
+    "ticketOwnerId": "1330898742110457962",
+    "ticketName": "youtuber・waiting-3",
+    "panelName": "Youtuber Apply",
+    "transcriptFile": "transcripts/transcript-youtuberwaiting-3.html",
+    "claimedBy": null,
+    "users": [
+      "<@1350531070222794804>",
+      "<@1330898742110457962>",
+      "<@722196398635745312>"
+    ],
+    "ticketOwnerName": "mhmdnemer",
+    "ticketOwnerDisplay": "Mhmdnemer"
+  },
+  {
     "timestamp": "2026-03-24T16:02:59.962Z",
     "ticketOwner": "<@1151593879989129227>",
     "ticketOwnerId": "1151593879989129227",
